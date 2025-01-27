98d6a6ea250db597f6437ffdbc302e8dfd551db7
Jason Em <jasonem@umail.ucsb.edu>
2025-01-26 17:34:41 -0800

    Complete exercise 05
    Used for loop to add all ints from smaller to larger input
    Added checks to error unwanted inputs

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let output = 0;
+    if (!Number.isInteger(int1) || !Number.isInteger(int2) || int1 < 0 || int2 < 0){
+        return "ERROR";
+    }
+    let lower, upper;
+    if (int1 < int2){
+        lower = int1;
+        upper = int2;
+    } else {
+        lower = int2;
+        upper = int1
+    }
+    for (let i = lower; i <= upper; i++){
+        output += i;
+    }
+    return output;
 };
 
 // Do not edit below this line

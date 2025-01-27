f019486ed64f6b3508b68b6ad35428f6b6bc56f1
Jason Em <jasonem@umail.ucsb.edu>
2025-01-26 16:49:20 -0800

    Complete exercise 02
    Made function in repeatString.js give error when negative number
    given in parameter or repeat given string parameter equal to
    number parameter. Removed .skip for tests to check all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numberOfRepeat) {
+    if (numberOfRepeat < 0){
+        return "ERROR";
+    }
+    let output = "";
+    for (let i = 0; i < numberOfRepeat; i++){
+        output += stringToRepeat;
+    }
+    return output;
 };
 
 // Do not edit below this line

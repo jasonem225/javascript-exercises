c408be488b47c60a2434a5fae6fdedfd7c32fb13
Jason Em <jasonem@umail.ucsb.edu>
2025-01-26 17:59:22 -0800

    Complete exercise 07
    Converted from fahrenheit to celcius ot vice versa
    Rounded value to 1 decimal and changed to Number to return

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius = Number(((fahrenheit - 32) * 5 / 9).toFixed(1));
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = Number(((celcius * 9 / 5) + 32).toFixed(1));
+  return fahrenheit;
 };
 
 // Do not edit below this line

32022c7b7e47092cd2026cd8c57e0efa067854d7
Jason Em <jasonem@umail.ucsb.edu>
2025-01-26 17:24:45 -0800

    Complete exercise 04
    Used for ... of loop to loop through multiple parameters
    used while loop with indexOf and splice to remove all given strings
    from given array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...stringsToRemove) {
+    let output = array;
+    let index;
+    for (let removeString of stringsToRemove){
+        index = output.indexOf(removeString);
+        while (index > -1){
+            output.splice(index, 1);
+            index = output.indexOf(removeString);
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line

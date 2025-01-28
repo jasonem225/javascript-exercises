11f670a4d20b64239ef179a748e2a78d1e345dd0
Jason Em <jasonem@umail.ucsb.edu>
2025-01-28 15:16:42 -0800

    Complete exercise 08
    Added functionality to functions: add, subtract, sum, multiply, power,
    factorial
    Removed .skips to enable given tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(integers) {
+  let sum = 0;
+	for (let integer of integers){
+    sum += integer;
+  }
+  return Number(sum);
 };
 
-const multiply = function() {
-
+const multiply = function(integers) {
+  let output = 1;
+	for (let integer of integers){
+    output *= integer;
+  }
+  return output;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let output = 1;
+	for (let i = 2; i <= x; i++){
+    output = output * i;
+  }
+  return output;
 };
 
 // Do not edit below this line

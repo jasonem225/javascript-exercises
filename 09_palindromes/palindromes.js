bcc0603beb999f1e1897eeb62ada00381cfbab22
Jason Em <jasonem@umail.ucsb.edu>
2025-02-11 18:57:17 -0800

    Complete exercise 09
    First made string into all lowercase, broke string into array,
    removed all non-letter and non-number chars, and put back together
    Made a reverse copy to compare to see if palindrome or not

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const lettersAndNumbers = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    let cleanString = str
+    .toLowerCase()
+    .split("")
+    .filter((char) => lettersAndNumbers.includes(char))
+    .join("");
+
+    const reversedString = cleanString
+    .split("")
+    .reverse()
+    .join("");
+
+    return (cleanString == reversedString)
 };
 
 // Do not edit below this line

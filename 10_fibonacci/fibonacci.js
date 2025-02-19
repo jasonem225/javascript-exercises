9107cb23313ed8cb158667c4e2e09b4ba654c453
Jason Em <jasonem@umail.ucsb.edu>
2025-02-18 17:19:46 -0800

    Complete exercise 10
    First added base cases to remove negative and zero input.
    Then created loop to go through fib until wanted number in sequence
    was found.
    Removed test skips in given tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(input){
+    if (input == 0){
+        return 0;
+    }else if (input < 0){
+        return "OOPS";
+    }
+    let arr = [0, 1];
+    for (let i = 1; i < input; i++){
+        let temp = arr.reduce((sum, currentItem)=> {
+            return sum + currentItem;
+        }, 0);
+        arr.splice(0, 1);
+        arr.push(temp);
+    }
+    return arr[1];
 };
 
 // Do not edit below this line

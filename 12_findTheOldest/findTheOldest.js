186318f25231808690379c8c8f10f2b1bf624e04
Jason Em <jasonem@umail.ucsb.edu>
2025-02-18 17:59:57 -0800

    Complete exercise 12
    Used getFullYear function to get ages of people
    Used map to go through all people in array
    return oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,9 +1,19 @@
 const findTheOldest = function(people) {
-    let oldest = people[0]
+    const date = new Date();
+    const year = date.getFullYear();
+    let oldest = people[0];
+    if (!('yearOfDeath' in oldest)){
+        oldest.yearOfDeath = year;
+    }
     let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
     people.map((person) => {
+        if (!('yearOfDeath' in person)){
+            person.yearOfDeath = year;
+        }
         let tempAge = person.yearOfDeath - person.yearOfBirth;
+        console.log(person.name + " " + tempAge);
         if (tempAge > oldestAge){
+            console.log("New oldest");
             oldest = person;
             oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
         } 

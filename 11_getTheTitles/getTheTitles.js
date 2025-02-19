113948d66b356ffd77dda1c794b31726951017f6
Jason Em <jasonem@umail.ucsb.edu>
2025-02-18 17:28:05 -0800

    Complete exercise 11
    Used map function with onject keys to get only book titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let arr = [];
+    books.map((book) => arr.push(book.title));
+    return arr;
 };
 
 // Do not edit below this line

624a1f6fd7e1609533b94b85b4a9c184f888a68f
Jason Em <jasonem@umail.ucsb.edu>
2025-02-18 17:48:57 -0800

    Exercise 12 test 1 pass
    Used object keys to find age and find oldest person in given array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people[0]
+    let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    people.map((person) => {
+        let tempAge = person.yearOfDeath - person.yearOfBirth;
+        if (tempAge > oldestAge){
+            oldest = person;
+            oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        } 
+    });
+    return oldest;
 };
 
 // Do not edit below this line
